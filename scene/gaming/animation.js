f0c4edaa5bdde557f69cf1d40abe1c2673726a8c
Haobo-Zhao <haobo4608@gmail.com>
2018-12-21 14:48:47 -0500

    optimized implementation of animation

--- scene/gaming/animation.js
+++ scene/gaming/animation.js
@@ -1,15 +1,13 @@
 class Animation {
     constructor(game, name) {
         this.game = game
-        // this.frames 里面装的是 
-        this.frames = []
+        // this.textures 里面装的是 
+        this.textures = []
         for (let i = 0; i <= 9; i++) {
-            let animationName = name + i
-            let frame = {}
-            frame.texture = game.imageByName(animationName)
-            frame.x = this.x
-            frame.y = this.y
-            this.frames.push(frame)
+            // let animationName = name + i
+            let animationName = `${name}${i}`
+            let frame = game.imageByName(animationName)
+            this.textures.push(frame)
         }
         this.setup()
     }
@@ -19,39 +17,27 @@ class Animation {
     }
 
     setup() {
-        // 用动画这个类的 x 和 y，去给每一个 frame 的图像定位
         this.x = 100
         this.y = 20
-        this.cooldown = 12
-        this.frameIndex = 0
-        this.frame = this.frames[0]
-        this.setupFrame()
-        
-    }
-
-    setupFrame() {
-        // frame 的位置由 animation 的位置来决定
-        this.frame.x = this.x
-        this.frame.y = this.y
-        this.w = this.frame.width
-        this.h = this.frame.height
+        this.cooldown = 3
+        this.textureIndex = 0
+        this.texture = this.textures[0]
     }
 
     update() {
         this.cooldown--
         if (this.cooldown === 0) {
-            this.cooldown = 12
-            this.frameIndex = (this.frameIndex + 1) % this.frames.length
-            this.frame = this.getFrame()
-            this.setupFrame()
+            this.cooldown = 3
+            this.textureIndex = (this.textureIndex + 1) % this.textures.length
+            this.texture = this.getFrame()
         }
     }
 
     draw() {
-        this.game.drawImage(this.frame)
+        this.game.drawImage(this)
     }
 
     getFrame() {
-        return this.frames[this.frameIndex]
+        return this.textures[this.textureIndex]
     }
 }
