ed10165a87a66b615e2df3ed238825938fb503fb
Haobo-Zhao <haobo4608@gmail.com>
2019-07-26 18:36:48 -0400

    refactor player

--- season2/ep1/scene/gaming/player.js
+++ season2/ep1/scene/gaming/player.js
@@ -1,15 +1,10 @@
 class Player extends Element {
     constructor(game, x, y) {
         super(game, 'player', x, y)
-        this.init()
-    }
-
-    static new(game, x, y) {
-        this.i = this.i || new this(game, x, y)
-        return this.i
     }
 
     init() {
+        super.init()
         this.x = 100
         this.y = 200
         this.speed = 10
@@ -17,12 +12,12 @@ class Player extends Element {
 
     moveByX(dx) {
         dx = dx || 0
-        this.x = clamp(this.x + dx, 0, 400 - this.w)
+        this.x = clamp(this.x + dx, 0, 800 - this.w)
     }
 
     moveByY(dy) {
         dy = dy || 0
-        this.y = clamp(this.y + dy, 0, 300 - this.h)
+        this.y = clamp(this.y + dy, 0, 1000 - this.h)
     }
 
     moveUp() {
