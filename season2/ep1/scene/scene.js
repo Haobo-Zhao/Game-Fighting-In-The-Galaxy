56cb09faef445044f840df4e35d17cde4517d70b
Haobo-Zhao <haobo4608@gmail.com>
2019-07-26 17:46:02 -0400

    refactor scene

--- season2/ep1/scene/scene.js
+++ season2/ep1/scene/scene.js
@@ -1,6 +1,7 @@
 class Scene {
     constructor(game) {
         this.game = game
+        this.elemenets = []
     }
 
     static new(game) {
@@ -12,12 +13,20 @@ class Scene {
         return this.i
     }
 
+    addElement(e) {
+        this.elemenets.push(e)
+    }
+
     // purposefully default update method to be empty
     update() {
-
+        for (const e of this.elemenets) {
+            e.update()
+        }
     }
     // purposefully default draw method to be empty
     draw() {
-
+        for (const e of this.elemenets) {
+            this.game.drawElement(e)
+        }
     }
 }
