d32dddfdb68aa0b52124aa0b3ff02222663385c9
Haobo-Zhao <haobo4608@gmail.com>
2019-07-26 18:05:17 -0400

    refactor element

--- season2/ep1/scene/gaming/element.js
+++ season2/ep1/scene/gaming/element.js
@@ -3,6 +3,8 @@ class Element {
         this.texture = game.textureByName(name)
         this.x = x || 0
         this.y = y || 0
+        this.w = this.texture.width
+        this.h = this.texture.height
     }
 
     // static  函数的 this， 是绑定在调用 static 函数的类，身上的
@@ -12,6 +14,7 @@ class Element {
         return this.i
     }
 
+    // purposefully empty currently
     init() {
 
     }
